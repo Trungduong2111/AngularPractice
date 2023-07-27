c9856fd256ad3e442afbd340cc6ce434f8727dd4
“vphamthe” <vphamthe@avaya.com>
2023-07-27 09:48:35 +0700

    Update4

--- ANGUlLAR/src/app/app.module.ts
+++ ANGUlLAR/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponentComponent } from './course-component/course-component.component';
+import { TemplateComponent } from './template/template.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponentComponent
+    CourseComponentComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
